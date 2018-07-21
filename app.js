a50fef902eff0ccfdbdac8b6d28bed0f2677c904
maximo <maximodleon@gmail.com>
2018-07-21 09:07:08 -0400

    add vim swp files to ignore, use logging middleware

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const { BOT_TOKEN } = process.env
 
 const bot = new Telegraf(BOT_TOKEN)
-
+bot.use(Telegraf.log())
 bot.start((ctx) => ctx.reply('Hola!'))
 commands.initCommands(bot)
 
