a164d02cc44bf1d3c31cdebd187afd9e3ef95f0b
maximo <maximodleon@gmail.com>
2018-07-29 19:24:26 -0400

    refactor(commands/clima.js): Fix inline keyboard layout
    Fix inline keyboard layout for the case when several cities are named the same for different
    countries

--- commands/clima.js
+++ commands/clima.js
@@ -8,8 +8,8 @@ const executeCommand = async (ctx) => {
   const searchCity = ctx.message.text.replace('/clima', '').trim().toLowerCase()
   const cities = cityList.filter((city) => city.name.toLowerCase() === searchCity)
   if (cities.length > 1) {
-    const results = cities.map((city) => [Markup.callbackButton(city.country, `weather:${city.id}`)])
-    const keyboard = Markup.inlineKeyboard(results)
+    const results = cities.map((city) => Markup.callbackButton(city.country, `weather:${city.id}`))
+    const keyboard = Markup.inlineKeyboard(getSlicedArray(results))
     ctx.reply('de que pais', keyboard.resize().extra())
   } else if (cities.length === 1) {
     await weatherService.generateWeatherScreenshotForCity(cities[0].id)
@@ -29,6 +29,15 @@ const getWeatherForCity = (bot) => {
     await weatherService.generateWeatherScreenshotForCity(cityId)
     await ctx.replyWithPhoto({ source: 'weather.png' })
   })
+}
+
+const getSlicedArray = (resultsArray) => {
+ return resultsArray.reduce((result, value, index, array) => {
+   if(index%2===0) {
+   result.push(array.slice(index, index + 2))
+  }
+ return result
+}, [])
 }
 
 module.exports = {
