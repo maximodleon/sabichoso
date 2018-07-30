895a83d2006f8aa44b839babf08b767372f75252
maximo <maximodleon@gmail.com>
2018-07-30 19:20:33 -0400

    refactor(commands/gas): move logic to service
    move gas prices scraping to shared service
    
    fix #11

--- commands/gasolina.js
+++ commands/gasolina.js
@@ -1,7 +1,4 @@
-const browserHelper = require('../helpers/browser-helper.js')
-const puppeteer = require('puppeteer')
-const { GAS_PRICE_URL } = require('../constants/index.js')
-const templateHelper = require('../helpers/templates-helper')
+const gasService = require('../services/gas')
 
 require('dotenv').config()
 const { BOT_NAME } = process.env
@@ -14,23 +11,9 @@ const getGasPrices = (bot) => {
 const executeCommand = async (ctx) => {
   ctx.reply('déjame buscarlo y te lo mando...')
   console.log('ejecutando el comando gasolina...')
-  const browser = await puppeteer.launch({ args: ['--start-maximized'] })
-  const { table, caption } = await getTableAndCaption(browser)
-  const template = templateHelper.loadAndRenderTemplate('gas', { table, caption })
-  await browserHelper.generateScreenshot(browser, template, 'div', 2, 'gas.png')
+  gasService.generateGasPriceScreenshot()
   console.log('respondiendo')
   ctx.replyWithPhoto({ source: 'gas.png' }, { caption: 'Aqui están los precios.' })
-  await browser.close()
-}
-
-const getTableAndCaption = async (browser) => {
-  const page = await browser.newPage()
-  browserHelper.setupPageLoading(page)
-  await page.goto(GAS_PRICE_URL)
-  const table = await page.evaluate(() => document.querySelector('table > tbody').outerHTML)
-  const caption = await page.evaluate(() => document.querySelector('h2').innerHTML)
-  await page.close()
-  return { table, caption }
 }
 
 module.exports = {
---
+++ services/gas.js
@@ -0,0 +1,26 @@
+const browserHelper = require('../helpers/browser-helper.js')
+const puppeteer = require('puppeteer')
+const templateHelper = require('../helpers/templates-helper')
+const { GAS_PRICE_URL } = require('../constants/index.js')
+
+const generateGasPriceScreenshot = async () => {
+  const browser = await puppeteer.launch({ args: ['--start-maximized'] })
+  const { table, caption } = await getTableAndCaption(browser)
+  const template = templateHelper.loadAndRenderTemplate('gas', { table, caption })
+  await browserHelper.generateScreenshot(browser, template, 'div', 2, 'gas.png')
+  await browser.close()
+}
+
+const getTableAndCaption = async (browser) => {
+  const page = await browser.newPage()
+  browserHelper.setupPageLoading(page)
+  await page.goto(GAS_PRICE_URL)
+  const table = await page.evaluate(() => document.querySelector('table > tbody').outerHTML)
+  const caption = await page.evaluate(() => document.querySelector('h2').innerHTML)
+  await page.close()
+  return { table, caption }
+}
+
+module.exports = {
+generateGasPriceScreenshot
+}
