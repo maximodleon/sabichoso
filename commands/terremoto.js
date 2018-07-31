c86038307d9f23206ed049def8ee05c456862ebf
maximo <maximodleon@gmail.com>
2018-07-30 20:46:08 -0400

    refactor(earthquake): Write earthquake service
    decouple earthquake lookup form command by creating a service
    
    fix #14

--- commands/terremoto.js
+++ commands/terremoto.js
@@ -1,13 +1,6 @@
 require('dotenv').config()
-const axios = require('axios')
-
-const { EARTHQUAKE_API_URL, MIN_EARTHQUAKE_MAGNITUDE, BOT_NAME } = process.env
-
-/* DR coordinates */
-const LATITUDE = 18.735693
-const LONGITUDE = -70.162651
-const MAX_RADIUS = 5
-const FORMAT = 'geojson'
+const earthquakeService = require('../services/earthquake')
+const { BOT_NAME } = process.env
 
 const getEarthquakeInfo = (bot) => {
   const commands = ['terremoto', `terremoto@${BOT_NAME}`, 'terremotos', `terremtos@${BOT_NAME}`]
@@ -15,53 +8,15 @@ const getEarthquakeInfo = (bot) => {
 }
 
 const executeCommand = async (ctx) => {
-  const dates = getDates()
-  const params = {
-    format: FORMAT,
-    longitude: LONGITUDE,
-    latitude: LATITUDE,
-    maxradius: MAX_RADIUS,
-    minmagnitude: MIN_EARTHQUAKE_MAGNITUDE,
-    starttime: dates.today,
-    endtime: dates.tomorrow
-  }
-  const results = await axios.get(EARTHQUAKE_API_URL, { params: { ...params } })
-  const filtered = results.data.features.filter((feature) => feature.properties.place.includes('Dominican Republic'))
-  if (filtered.length) {
-    const { properties } = filtered[0]
-    const details = await axios.get(properties.detail)
-    const { data } = details
-    const place = properties.place.substring(properties.place.indexOf('of') + 3)
-    const mapImage = data.properties.products.dyfi[0].contents[`us${properties.code}_ciim.jpg`]
-    ctx.replyWithPhoto({ url: mapImage.url },
+  const results = earthquakeService.getEarthquakeInfo()
+  if (results.length) {
+    ctx.replyWithPhoto({ url: results.mapImage },
       {
-        caption: `Terremoto de magnitud *${properties.mag}* en *${place}* para más detalles presiona [aqui](${properties.url})`,
+        caption: `Terremoto de magnitud *${results.magnitude}* en *${results.place}* para más detalles presiona [aqui](${results.detailsUrl})`,
         parse_mode: 'Markdown'
       })
   } else {
     ctx.reply('No han habido terremotos en las últimas 24 horas')
-  }
-}
-
-const getDates = () => {
-  const today = new Date()
-  const todayDay = getDatePart(today.getDay())
-  const todayMoth = getDatePart(today.getMonth())
-  const tomorrow = getDatePart(today.getDay() + 1)
-  const todayFormatted = `${today.getFullYear()}-${todayMoth}-${todayDay}`
-  const tomorrowFormatted = `${today.getFullYear()}-${todayMoth}-${tomorrow}`
-
-  return {
-    today: todayFormatted,
-    tomorrow: tomorrowFormatted
-  }
-}
-
-const getDatePart = (part) => {
-  if (part >= 10) {
-    return `${part}`
-  } else {
-    return `0${part}`
   }
 }
 
---
+++ services/earthquake.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+const axios = require('axios')
+const { EARTHQUAKE_API_URL, MIN_EARTHQUAKE_MAGNITUDE } = process.env
+
+/* DR coordinates */
+const LATITUDE = 18.735693
+const LONGITUDE = -70.162651
+const MAX_RADIUS = 5
+const FORMAT = 'geojson'
+
+const getEarthquakeInfo = async () => {
+  const dates = getDates()
+  const params = {
+    format: FORMAT,
+    longitude: LONGITUDE,
+    latitude: LATITUDE,
+    maxradius: MAX_RADIUS,
+    minmagnitude: MIN_EARTHQUAKE_MAGNITUDE,
+    starttime: dates.today,
+    endtime: dates.tomorrow
+  }
+
+  const results = await axios.get(EARTHQUAKE_API_URL, { params: { ...params } })
+  const filtered = results.data.features.filter((feature) => feature.properties.place.includes('Dominican Republic'))
+
+  const value = []
+  if (filtered.length) {
+    const { properties } = filtered[0]
+    const details = await axios.get(properties.detail)
+    const { data } = details
+    const place = properties.place.substring(properties.place.indexOf('of') + 3)
+    const mapImage = data.properties.products.dyfi[0].contents[`us${properties.code}_ciim.jpg`]
+    const info = {
+      magnitude: properties.mag,
+      place,
+      detailsUrl: properties.url,
+      mapImage: mapImage.url
+    }
+    value.push(info)
+  }
+
+  return value
+}
+
+const getDates = () => {
+  const today = new Date()
+  const todayDay = getDatePart(today.getDay())
+  const todayMoth = getDatePart(today.getMonth())
+  const tomorrow = getDatePart(today.getDay() + 1)
+  const todayFormatted = `${today.getFullYear()}-${todayMoth}-${todayDay}`
+  const tomorrowFormatted = `${today.getFullYear()}-${todayMoth}-${tomorrow}`
+
+  return {
+    today: todayFormatted,
+    tomorrow: tomorrowFormatted
+  }
+}
+
+const getDatePart = (part) => {
+  if (part >= 10) {
+    return `${part}`
+  } else {
+    return `0${part}`
+  }
+}
+
+module.exports = {
+  getEarthquakeInfo
+}
