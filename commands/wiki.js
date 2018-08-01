65d2b4c84c5a4d934818a930abc889fe973604d9
maximo <maximodleon@gmail.com>
2018-08-01 19:31:06 -0400

    refactor(commands/wiki): refactor command code
    create executeCommand to handle command logic, use destructuring, fix regex that matches action

--- commands/wiki.js
+++ commands/wiki.js
@@ -1,28 +1,33 @@
 const wiki = require('wikipediajs')
 const Markup = require('telegraf/markup')
 
-const BASE_URL = 'https://es.wikipedia.org/wiki?curid='
+const executeCommand = async (ctx) => {
+  const { message: { text } } = ctx
+  const word = text.substr(text.indexOf(' '))
+  const { query: { pages } } = await wiki.search(word, 'es')
+  if (!pages) return ctx.reply('no hay resultados para esa busqueda')
+
+  const keys = Object.keys(pages)
+  if (keys.length === 1) {
+    ctx.reply(pages[keys[0]].fullurl)
+  } else if (keys.length > 1) {
+    const buttons = keys.map((key) => {
+      const data = `wiki:${pages[key].title.replace(/\s/g, '_')}`
+      const text = pages[key].title
+      return [Markup.callbackButton(text, data)]
+    })
+    const keyboard = Markup.inlineKeyboard(buttons)
+    ctx.reply('encontre varios, cual de estos?', keyboard.resize().extra())
+  }
+}
+
 const getWikiArticles = (bot) => {
-  bot.command('wiki', async (ctx) => {
-    const word = ctx.message.text.substr(ctx.message.text.indexOf(' '))
-    const { query } = await wiki.search(word, 'es')
-    if (!query.pages) return ctx.reply('no hay resultados para esa busqueda')
+  bot.command('wiki', executeCommand)
 
-    const keys = Object.keys(query.pages)
-    if (keys.length === 1) {
-      ctx.reply(query.page[keys[0]].fullurl)
-    } else if (keys.length > 1) {
-      const buttons = keys.map((key) => [Markup.callbackButton(query.pages[key].title, `wiki:${key}`)])
-      const keyboard = Markup.inlineKeyboard(buttons)
-      ctx.message.state = { pages: query.pages }
-      ctx.reply('encontre varios, cual de estos?', keyboard.resize().extra())
-    }
-  })
-
-  bot.action(/wiki:(\d+)/g, async (ctx) => {
-    const articleId = ctx.match[1]
+  bot.action(/wiki:([a-zA-Z0-9_.()áéíóú]+)/gi, async (ctx) => {
+    const article = ctx.match[1]
     await ctx.deleteMessage()
-    await ctx.reply(`${BASE_URL}${articleId}`)
+    await ctx.reply(`https://es.wikipedia.org/wiki/${article}`)
   })
 }
 
