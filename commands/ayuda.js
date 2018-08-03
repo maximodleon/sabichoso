2a83faa76ea5a2174dbb2473810edc95befc0a1b
maximo <maximodleon@gmail.com>
2018-08-03 19:30:46 -0400

    fix(help): keep help text in md file, add wiki command to help text
    fix #45 #54

--- commands/ayuda.js
+++ commands/ayuda.js
@@ -1,21 +1,13 @@
 require('dotenv').config()
 const { BOT_NAME } = process.env
+const fs = require('fs')
 
 const getHelp = (bot) => {
   const commands = ['ayuda', `ayuda@${BOT_NAME}`]
   bot.command(commands, (ctx) => {
-    ctx.replyWithMarkdown(`
-   Yo puedo ayudarte a buscar diferentes informaciones al escribir alguno de los siguientes comandos:
-
-
-   */clima:* _Escribe una ciudad del mundo y recibe el estado actual del clima. Ejemplo:_ */clima santo domingo*
-
-   */combustible o /gasolina:* _Recibir los precios actuales de los combustibles en RD_
-
-   */frase:* _Recibir frase de motivación al azar_
-
-   */terrmoto:* _Recibir información sobre terremotos occuridos en RD en las útlimas 24 horas, si es que ha habido alguno con magnitud >= 4.5_
-  `)
+    const helpText = fs.readFileSync('assets/help.md', 'utf-8')
+    console.log(helpText)
+    ctx.replyWithMarkdown(helpText)
   })
 }
 
