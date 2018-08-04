5a70cf487c2348e855899ef99c560c3bfa3abeaa
maximo <maximodleon@gmail.com>
2018-08-04 09:29:20 -0400

    refactor(command/terremoto): lint

--- commands/terremoto.js
+++ commands/terremoto.js
@@ -9,9 +9,9 @@ const getEarthquakeInfo = (bot) => {
 
 const executeCommand = async (ctx) => {
   const magnitude = ctx.message.text.split(' ')[1]
-	if (magnitude) {
-   if (Number.parseFloat(magnitude).toString() === Number.NaN.toString()) return ctx.reply('Tienes que introducir un número para la magnitud')
-	}
+  if (magnitude) {
+    if (Number.parseFloat(magnitude).toString() === Number.NaN.toString()) return ctx.reply('Tienes que introducir un número para la magnitud')
+  }
   const results = await earthquakeService.getEarthquakeInfo(magnitude)
   if (results.length) {
     const caption = results.map((earthquake) => `Terremoto de magnitud *${earthquake.magnitude}* en *${earthquake.place}* para más detalles presiona [aqui](${earthquake.detailsUrl})`).join('\n\n')
