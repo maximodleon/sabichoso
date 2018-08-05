25722fcccfc50803eaba4a1ec862dc044bc3cfa9
maximo <maximodleon@gmail.com>
2018-08-04 23:57:00 -0400

    feat(movies): [WIP] add movie scraping service, need to add code for command
    fix #20

---
+++ services/movie-scraping.js
@@ -0,0 +1,80 @@
+const { parseString } = require('xml2js')
+const { get } = require('axios')
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+
+const scrapeMovieInfo = async () => {
+  const browser = await puppeteer.launch()
+  const feedItems = await getRSS()
+
+  const movies = await Promise.all(
+    feedItems.map(async (item) => {
+      const page = await browser.newPage()
+      await page.goto(item.link[0])
+
+      const movieDetail = await page.evaluate(evaluateMoviePage)
+
+      const movie = {
+        title: item.title[0],
+        trailer: item.guid[0],
+        link: item.link[0],
+        estreno: item.category[0] === 'ESTRENO',
+        poster: item.enclosure[0].$.url,
+        showings: movieDetail
+      }
+
+      return movie
+    })
+  )
+
+  await browser.close()
+  fs.writeFileSync('movies.json', JSON.stringify(movies))
+}
+
+const getRSS = async () => {
+  const { data } = await get('http://www.cinema.com.do/rss.php')
+  let feed
+  parseString(data, (error, json) => {
+    if (error) throw error
+    feed = json
+  })
+
+  return feed.rss.channel[0].item
+}
+
+const evaluateMoviePage = () => {
+  const divs = document.querySelectorAll('.panel>div.row')
+  const theaters = {}
+  let dayTime = []
+  for (let div of divs) {
+    const divParent = div.querySelector('div[class="small-12 medium-12 large-12 columns"]')
+    const movieTheater = divParent.querySelector('h6').innerText
+    const times = divParent.querySelectorAll('div[class="row collapse"]>div[class="small-12 medium-11 large-11 columns"]>ul>li')
+    for (let time of times) {
+      const ps = time.querySelectorAll('p')
+      const day = { hours: [] }
+      for (let p of ps) {
+        if (['Jue', 'Vie', 'Sab', 'Dom', 'Lun', 'Mar', 'Mie'].includes(p.innerText)) {
+          day.name = p.innerText
+        } else {
+          day.hours.push(p.innerText)
+        }
+      }
+
+      const found = dayTime.findIndex((d) => d.name === day.name)
+      if (found > -1) {
+        dayTime[found].hours = dayTime[found].hours.concat(day.hours)
+      } else {
+        dayTime.push(day)
+      }
+    }
+
+    theaters[movieTheater] = dayTime
+    dayTime = []
+  }
+  return theaters
+}
+
+module.exports = {
+  scrapeMovieInfo
+}
