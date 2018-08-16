fe1974b104cb824f37f3fdb27a2ae47839d8f26b
maximo <maximodleon@gmail.com>
2018-08-16 17:24:31 -0400

    fix(commands/wiki): Cache wiki results
    fixes #68

--- commands/wiki.js
+++ commands/wiki.js
@@ -1,15 +1,35 @@
 const wiki = require('wikipediajs')
 const Markup = require('telegraf/markup')
+const memoize = require('memoizee')
 
-const executeCommand = async (ctx) => {
-  const { message: { text } } = ctx
+/* maxAge = one week (7 days)
+ * 3600 * 1000 = one hour in miliseconds
+ * a day has 24 hours
+ * and a week has 7 days
+ * which gives 3600 * 100 * 24 * 7
+ * to get 1 week of caching
+ */
+const maxAge = 3600 * 1000 * 24 * 7
+
+const _searchWiki = async word => {
+  return wiki.search(word, 'es')
+}
+
+const searchWiki = memoize(_searchWiki, { promise: true, maxAge })
+
+const executeCommand = async ctx => {
+  const {
+    message: { text }
+  } = ctx
   const word = text.substr(text.indexOf(' '))
   let pages
   try {
-    const { query } = await wiki.search(word, 'es')
+    const { query } = await searchWiki(word)
     pages = query.pages
   } catch (error) {
-    ctx.reply('oops..Ha habido un error buscando la información. Por favor intenta en unos minutos')
+    ctx.reply(
+      'oops..Ha habido un error buscando la información. Por favor intenta en unos minutos'
+    )
     throw error
   }
   if (!pages) return ctx.reply('no hay resultados para esa búsqueda')
@@ -18,7 +38,7 @@ const executeCommand = async (ctx) => {
   if (keys.length === 1) {
     ctx.reply(pages[keys[0]].fullurl)
   } else if (keys.length > 1) {
-    const buttons = keys.map((key) => {
+    const buttons = keys.map(key => {
       const data = `wiki:${pages[key].title.replace(/\s/g, '_')}`
       const text = pages[key].title
       return [Markup.callbackButton(text, data)]
@@ -28,14 +48,16 @@ const executeCommand = async (ctx) => {
   }
 }
 
-const getWikiArticles = (bot) => {
+const getWikiArticles = bot => {
   bot.command('wiki', executeCommand)
 
-  bot.action(/wiki:([a-zA-Z0-9_.()áéíóú]+)/gi, async (ctx) => {
+  bot.action(/wiki:([a-zA-Z0-9_.()áéíóú]+)/gi, async ctx => {
     const article = ctx.match[1]
     try {
       await ctx.deleteMessage()
-    } catch (error) { console.log('error deleting wiki artcile message') }
+    } catch (error) {
+      console.log('error deleting wiki artcile message')
+    }
     await ctx.reply(`https://es.wikipedia.org/wiki/${article}`)
   })
 }
