064c14696d9c71aab67e4770c0ff51d7d4dd1e10
maximo <maximodleon@gmail.com>
2018-08-19 21:04:00 -0400

    docs(helpers/pagination-helper): document functions

--- helpers/pagination-helper.js
+++ helpers/pagination-helper.js
@@ -1,5 +1,14 @@
 const Markup = require('telegraf/markup')
 
+/**
+ * Returns keyboard with paginated results from a given array of objects or strings
+ * @function paginateArray
+ * @param {Object [] | String []} array array to generate the keyboard from
+ * @param {int} offset index from where to slice the array
+ * @param {int} count amount of buttons/records to get from the array
+ * @param {Object} callbackButtonOtions configuration object with information to get keys from the array and set callback data
+ * @return {Markup.inlineKeyboard}  keyboard with keys and next/prev buttons
+ */
 const paginateArray = (
   array,
   offset = 0,
@@ -19,6 +28,14 @@ const paginateArray = (
   return Markup.inlineKeyboard(keyboard)
 }
 
+/**
+ * Returns next and previous buttons for pagination
+ * @function getNextPrev
+ * @param {Object [] | String []} array array with the keys for the inline keyboard
+ * @param {int} offset index from where to slice the array
+ * @param {int} count how many elements to get from the array
+ * @param {String} callbackDataPrefix prefix for inline keyboard callback data
+ */
 const getNextPrev = (array, offset, count, callbackDataPrefix) => {
   let prev
   let next
@@ -43,6 +60,14 @@ const getNextPrev = (array, offset, count, callbackDataPrefix) => {
   return [buttons]
 }
 
+/**
+ * Generate callback buttons for a command
+ *
+ * @function getCallbackButtons
+ * @param {Object [] | String []} array array of element from where to get information for the buttons
+ * @params {Object} callbackButtonsOptions configuration object that tells how to get information from array
+ * @return {Object []} buttons for the inline keyboard
+ */
 const getCallbackButtons = (array, callbackButtonsOptions) => {
   const {
     callbackText,
