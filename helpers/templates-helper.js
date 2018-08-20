556c83cacce443357b3eea49a83b3c0373adce43
maximo <maximodleon@gmail.com>
2018-08-19 21:04:34 -0400

    docs(helpers/templates-helper): document functions

--- helpers/templates-helper.js
+++ helpers/templates-helper.js
@@ -2,7 +2,14 @@ const fs = require('fs')
 const Mustache = require('mustache')
 const TEMPLATES_DIR = 'assets/templates/'
 
-const loadTemplate = (templateName) => {
+/**
+ * returns template file/buffer for rendering
+ * @function loadTemplate
+ * @param {String} templateName name of template file to load
+ * @return {String | Buffer} contents of template file
+ *
+ */
+const loadTemplate = templateName => {
   try {
     return fs.readFileSync(`${TEMPLATES_DIR}${templateName}.template`, 'utf8')
   } catch (error) {
@@ -11,6 +18,13 @@ const loadTemplate = (templateName) => {
   }
 }
 
+/**
+ * Use mustache to render template loaded
+ * @function renderTemplate
+ * @param {String} template name of template to render
+ * @param {Object}  params parameters to send to template for rendering
+ * @return {Strinng} rendered templated
+ */
 const renderTemplate = (template, params) => {
   try {
     return Mustache.render(template, params)
@@ -20,6 +34,13 @@ const renderTemplate = (template, params) => {
   }
 }
 
+/**
+ * Load tempate from templates folder and render using mustache
+ * @function loadAndRenderTemplate
+ * @param {String} templateName name of template to render
+ * @param {Object} param paramters to send to teamplte for rendering
+ * @return {String} rendered template
+ */
 const loadAndRenderTemplate = (templateName, params) => {
   const template = loadTemplate(templateName)
   return renderTemplate(template, params)
