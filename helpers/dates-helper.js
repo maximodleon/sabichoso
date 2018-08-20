69ae5e1fbd8bc70a5d338a6ae5db8b367e0064a2
maximo <maximodleon@gmail.com>
2018-08-19 21:02:58 -0400

    docs(helpers/dates-helper): document functions

--- helpers/dates-helper.js
+++ helpers/dates-helper.js
@@ -1,10 +1,43 @@
-const getDayString = (day) => {
-  const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+/**
+ * Get name of day of week
+ * @function getDayString
+ * @param {int} day day of week from Date.prototype.getDay()
+ * @return {String} Name of day in spanish
+ */
+const getDayString = day => {
+  const days = [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes',
+    'Sábado'
+  ]
   return days[day]
 }
 
-const getMonthString = (month) => {
-  const days = ['Enero', 'Febreo', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septimebre', 'Octubre', 'Noviembre', 'Diciembre']
+/**
+ * Get name of month in spanish
+ * @function getMonthString
+ * @param {int} month month from Date.prototype.getMonth()
+ * @return {String} Name of month in spanish
+ */
+const getMonthString = month => {
+  const days = [
+    'Enero',
+    'Febreo',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septimebre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+  ]
   return days[month]
 }
 
