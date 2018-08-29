66a751dc6870ee19de6158c30908cefd6eb3df67
maximo <maximodleon@gmail.com>
2018-08-28 21:19:41 -0400

    feat(jobs/gas): add job to retrieve gas prices

---
+++ jobs/gas.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const { CronJob } = require('cron')
+const gasService = require('../services/gas')
+const Telegraf = require('telegraf')
+const fs = require('fs')
+const { BOT_TOKEN, LOG_CHAT_ID } = process.env
+
+const job = new CronJob('0 0 12 * * *', async () => {
+  console.log('getting gas prices', new Date().toLocaleString())
+
+  const bot = new Telegraf(BOT_TOKEN)
+  let filename
+  try {
+    filename = await gasService.generateGasPriceScreenshot()
+  } catch (error) {
+    console.log('error getting gas prices', error)
+    return
+  }
+
+  try {
+    await bot.telegram.sendPhoto(LOG_CHAT_ID, {
+      source: fs.readFileSync(filename)
+    })
+  } catch (error) {
+    console.log('error sending gas prices', error)
+    return
+  }
+
+  console.log('successfully sent gas prices')
+})
+
+job.start()
